1337f2ca23b39fb3c0d25f55ac0c36b1efcccd71
Alex Magana <gatobumalex@gmail.com>
2016-06-17 15:12:46 +0300

    Add code for andelabs
    This commit enatils code for exercises as seen in Andelabs

--- notesapplication.py
+++ notesapplication.py
@@ -8,21 +8,21 @@ class NotesApplication(object):
 	
 	def list(self):
 		for position in range(len(self.notes_list)):
-			print "Note ID: %f" % position
+			print "Note ID: %s" % position
 			print self.notes_list[position]
-			print "By Author %f" % self.author
+			print "By Author %s" % self.author
 	
 	def get(self, note_id):
 		return self.notes_list[note_id]
 		
 	def search(self, search_text):
-		print "Showing results for search %f" % search_text
+		print "Showing results for search %s" % search_text
 		for position in range(len(self.notes_list)):
 			idx = self.notes_list[position].find(search_text, 0, len(self.notes_list))
 			if idx >= 0:
-				print "Note ID: %f" % position
+				print "Note ID: %s" % position
 				print self.notes_list[position]
-				print "By Author %f" % self.author
+				print "By Author %s" % self.author
 		else:
 			print "No result found"
 	
